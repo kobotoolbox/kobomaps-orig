3215f4b763ca8acd8642516c48a17336e34e743f
tinok <tino.kreutzer@gmail.com>
2015-05-11 21:41:25 +0545

    First pass to new file structure, need to fix js

--- mapheader.php
+++ mapheader.php
@@ -1,31 +1,28 @@
 <?php
 /*
-*
-* This file is referenced from within Drupal in html--node--nnn.tpl.php
-* Any changes in this file will be visible in the Drupal across all fullscreen maps. 
-
-* ###The file name and file location should not be modified.###
-
-*
-*/
+ * KoBoMaps --- mapheader.php
+ * This file is referenced by each KoBoMap instance.
+ * Any changes in this file will be visible across all maps. 
+ * Map-specific header values should instead be entered into the respective map container file
+ */
 ?>
 
-<link href="/<?php echo kmapsubFolder ?>/css/largemap.css" type="text/css" rel="stylesheet"> 
+<link href="<?php echo kmapPath ?>/css/largemap.css" type="text/css" rel="stylesheet"> 
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript">google.load('visualization', '1', {packages: ['corechart']});</script>
 <script>
+
+// Only calling jquery if it's not already present. This helps with embedding kobomaps into Drupal. 
 if (typeof $ === 'undefined') {
-  document.write('<script type="text/javascript" src="/<?php echo kmapsubFolder ?>/js/jquery.js"><\/script>');
+  document.write('<script type="text/javascript" src="<?php echo kmapPath ?>/js/jquery.js"><\/script>');
 }
 </script>
-<script type="text/javascript" src="/<?php echo kmapsubFolder ?>/js/dataVisualize.js"> </script> 
-<script type="text/javascript" src="/<?php echo kmapsubFolder ?>/js/jquery.address-1.4.min.js"></script> 
-<script type="text/javascript" src="/<?php echo kmapsubFolder ?>/js/csvToArray.js"> </script>
-<script type="text/javascript" src="/<?php echo kmapsubFolder ?>/js/dragresize.js"> </script> 
+<script type="text/javascript" src="<?php echo kmapPath ?>/js/dataVisualize.js"></script> 
+<script type="text/javascript" src="<?php echo kmapPath ?>/js/jquery.address-1.4.min.js"></script> 
+<script type="text/javascript" src="<?php echo kmapPath ?>/js/csvToArray.js"> </script>
+<script type="text/javascript" src="<?php echo kmapPath ?>/js/dragresize.js"> </script> 
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"> </script>
  
-<script type="text/javascript" src="/<?php echo kmapsubFolder ?>/js/label.js"> </script> 
+<script type="text/javascript" src="<?php echo kmapPath ?>/js/label.js"> </script> 
 
-<?php //addthis button references and settings (site-specific!) ?>
-<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4e2cb8e90252e50b"> </script> 
-<script type="text/javascript" src="/<?php echo kmapsubFolder ?>/js/addthisbutton.js"> </script> 
+<title><?php echo $kmapPageTitle; ?></title>
