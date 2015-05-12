aab6f52565c2f0ed8aa30ca5d0cbe212eb5615c4
tinok <tino.kreutzer@gmail.com>
2015-05-12 07:40:02 +0545

    Update Google maps template

--- maps/google_spreadsheets_template.php
+++ maps/google_spreadsheets_template.php
@@ -1,4 +1,12 @@
 ﻿<?php
-//To pull CSV data from the new Google Spreadsheet format use the following:
-//echo file_get_contents("http://docs.google.com/spreadsheets/d/<documentID>/export?format=csv&id=<documentID>&gid=<sheetID>");
+	//Paste here the unique document ID and sheet ID from Google Spreadsheets
+	$googledoc_id 		= "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr";
+	$googledoc_sheet	= "1234567890";
+
+
+	//Don't change anything below here
+	$googledoc_url	= 'http://docs.google.com/spreadsheets/d/' . $googledoc_id . '/export?format=csv&id=' . $googledoc_id . '&gid='	. $googledoc_sheet ;
+
+	echo file_get_contents($googledoc_url);
+
 ?>
