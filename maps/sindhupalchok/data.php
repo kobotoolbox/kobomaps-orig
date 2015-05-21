6e3a3013385b99bce05e6ef48732e176ebfdaa9e
tinok <tino.kreutzer@gmail.com>
2015-05-21 10:25:12 -0400

    reverting data file

--- maps/sindhupalchok/data.php
+++ maps/sindhupalchok/data.php
@@ -1,4 +1,12 @@
-Level 1,Level 2,Level 3,Attarpur,Bandegaun,Bansbari,Banskharka,Baramchae,Barhabise,Baruwa,Batase,Bhimtar,Bhotsipa,Bhotenamlang,Bhotechaur,Chokati,Chautara,Dhuyang,Dubachaur,Phataksila,Phulchodanda,Phulpingkatti,Phulpingkot,Gati,Ghorthali,Gunsakun,Golche,Gumba,Ghunsakot,Hagam,Haibung,Helambu,Ichok,Irkhu,Jalbire,Jethal,Jyamire,Kadambas,Kalika,Karthali,Kiul,Kubhinde,Kunchok,Lagarche,Lisangkhu,Listokot,Mahangkal,Maneswar,Mangkha,Marming,Melamchi,Bhotang,Nawalpur,Pangretar,Palchok,Pangtang,Pedku,Pipaldanda,Piskar,Ramche,Sangachok,Sanusirubari,Selang,Shikharpur,Sindhukot,Sipapokharae,Sinpal Kavre,Sunkhani,Syaule,Talamarang,Tatopani,Tauthali,Tekanpur,Thakani,Ghuyang (Thanpalchap),Thapalkot,Thokarpa,Thulo Dhading,Thulo Pakhar,Thulo Sirubari,Thumpakhar,Yamuna Danda,TOTAL,Unit,Source,Source Link
-Caseload,Red Cross,Dead,1,103,0,142,26,17,220,115,79,90,58,38,4,143,115,160,,35,37,59,28,6,17,77,33,46,51,10,32,120,26,30,2,,81,22,21,80,,122,26,0,48,83,46,69,23,293,51,40,24,16,34,2,,18,16,153,,57,,,58,73,1,62,,14,13,6,35,113,49,8,0,3,72,16,11,3788,,Red Cross,
-,,Missing,0,0,,15,0,0,15,0,0,0,0,0,0,0,0,1,,0,0,0,0,0,0,0,0,5,0,15,10,11,0,0,0,,0,0,0,0,,0,3,0,0,0,0,0,0,24,18,0,0,2,0,0,,0,0,0,,0,,,0,0,0,0,,0,1,0,4,10,4,0,0,0,0,1,0,139,,Red Cross,
-,,Injured,0,0,,255,0,0,385,0,35,0,0,105,0,0,0,200,,0,0,0,4,0,11,0,0,93,0,200,50,246,0,0,2,,0,40,24,0,,0,205,0,0,200,45,0,0,1018,140,0,0,82,0,0,,0,50,0,,0,,,0,95,8,0,,0,8,0,65,255,109,1,0,0,0,3,0,3726,,Red Cross,
+<?php
+	//Paste here the unique document ID and sheet ID from Google Spreadsheets
+	$googledoc_id 		= "1Pl-C0tJRo3wWBXi8g9EfF85vTBSp8PF9B9z6HlqqwUE";
+	$googledoc_sheet	= "0";
+
+
+	//Don't change anything below here
+	$googledoc_url	= 'http://docs.google.com/spreadsheets/d/' . $googledoc_id . '/export?format=csv&id=' . $googledoc_id . '&gid='	. $googledoc_sheet ;
+
+	echo file_get_contents($googledoc_url);
+
+?>
