62444cb3bbb8d96482b52476aa35cc02b303152d
tinok <tino.kreutzer@gmail.com>
2016-06-01 19:06:26 +0200

    adding CI map

---
+++ maps/ci/data_p1.php
@@ -0,0 +1,13 @@
+﻿<?php
+	//Paste here the unique document ID and sheet ID from Google Spreadsheets
+	$googledoc_id 		= "19UdKQ5qGk5aYQkmP8hDuGlCncmFOZnvLVX1pgGD1hUY";
+	$googledoc_sheet	= "1450304081";
+	$googledoc_range	= "A2:L1000";
+
+
+	//Don't change anything below here
+	$googledoc_url	= 'http://docs.google.com/spreadsheets/d/' . $googledoc_id . '/export?format=csv&id=' . $googledoc_id . '&gid='	. $googledoc_sheet . '&range=' . $googledoc_range;
+
+	echo file_get_contents($googledoc_url);
+
+?>
