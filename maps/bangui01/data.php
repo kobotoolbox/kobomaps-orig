ca98015dbbef453ef4887ba15b71ab7150f0edec
tinok <tino.kreutzer@gmail.com>
2018-02-21 11:52:35 -0500

    adding four new CAR poll maps

---
+++ maps/bangui01/data.php
@@ -0,0 +1,13 @@
+﻿<?php
+	//Paste here the unique document ID and sheet ID from Google Spreadsheets
+	$googledoc_id 		= "1YZUZKczzf1Wc1EKcgqQy8qQf8J6XBt3lRy5WRC0vuNk";
+	$googledoc_sheet	= "1534832672";
+
+
+
+	//Don't change anything below here
+	$googledoc_url	= 'http://docs.google.com/spreadsheets/d/' . $googledoc_id . '/export?format=csv&id=' . $googledoc_id . '&gid='	. $googledoc_sheet;
+
+	echo file_get_contents($googledoc_url);
+
+?>
